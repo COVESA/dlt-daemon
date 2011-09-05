ca77a002e8012b82068f959c95fb1a57f7d41d6b
Christian Muck <christian.muck@bmw.de>
2011-09-05 16:19:48 +0200

    [GSW-16] Systemd configuration for syslog to DLT dapater

---
+++ systemd/dlt-syslog.service.cmake
@@ -0,0 +1,18 @@
+#  This file is for starting dlt-adaptor-udp
+#
+#  For more informations about starting options of dlt-daemon use the command "dlt-adaptor-udp -h".
+#
+#  basic.target A special target unit covering early boot-up.
+#  Usually this should pull-in all sockets, mount points, swap devices and 
+#  other basic initialization necessary for the general purpose daemons. 
+#  Most normal daemons should have dependencies of type After and Requires on this unit
+
+[Unit]
+Description=DLT Syslog Adapter
+
+[Service]
+ExecStart=/usr/local/bin/dlt-adaptor-udp -a @DLT_SYSLOG_APPID@ -c @DLT_SYSLOG_CTID@ -p @DLT_SYSLOG_PORT@
+# Restart=always
+
+[Install]
+WantedBy=basic.target
