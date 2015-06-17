accae3432f19176a51281dddf981c24919fc180d
Lutz Helwing <lutz_helwing@mentor.com>
2015-06-17 11:17:38 +0200

    Added licence and Doxygen headers
    Signed-off-by: Alexander Wenzel <Alexander.AW.Wenzel@bmw.de>

--- tests/gtest_dlt_common.cpp
+++ tests/gtest_dlt_common.cpp
@@ -1,3 +1,30 @@
+/*
+ * @licence app begin@
+ * SPDX license identifier: MPL-2.0
+ *
+ * Copyright (C) 2011-2015, BMW AG
+ *
+ * This file is part of GENIVI Project DLT - Diagnostic Log and Trace.
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License (MPL), v. 2.0.
+ * If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * For further information see http://www.genivi.org/.
+ * @licence end@
+ */
+
+/*!
+ * \author
+ * Stefan Held <stefan_held@mentor.com>
+ *
+ * \copyright Copyright © 2011-2015 BMW AG. \n
+ * License MPL-2.0: Mozilla Public License version 2.0 http://mozilla.org/MPL/2.0/.
+ *
+ * \file gtest_dlt_common.cpp
+*/
+
 #include <stdio.h>
 #include <gtest/gtest.h>
 #include <limits.h>
