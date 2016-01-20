e712b34212b6e0f03ec4843531ae2df03c53ec7d
Lutz Helwing <lutz_helwing@mentor.com>
2016-01-20 15:09:27 +0100

    Fixed core pattern to use correct dlt-cdh install path
    - 50-coredump.conf is now generated and installed
      by cmake and uses @CMAKE_INSTALL_PREFIX@ for setting
      path to dlt-cdh
    - README updated
    
    Signed-off-by: Lutz Helwing <lutz_helwing@mentor.com>

---
+++ src/core_dump_handler/50-coredump.conf.cmake
@@ -0,0 +1,18 @@
+#######
+# @licence make begin@
+# SPDX license identifier: MPL-2.0
+#
+# Copyright (C) 2011-2015, BMW AG
+#
+# This file is part of GENIVI Project DLT - Diagnostic Log and Trace.
+#
+# This Source Code Form is subject to the terms of the
+# Mozilla Public License (MPL), v. 2.0.
+# If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# For further information see http://www.genivi.org/.
+# @licence end@
+#######
+
+kernel.core_pattern=|@CMAKE_INSTALL_PREFIX@/bin/dlt-cdh %t %p %s %e
