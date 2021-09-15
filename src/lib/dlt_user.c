2e5c2e698b1c4a115f23ddf424c332e01aa910ab
Saya Sugiura <ssugiura@jp.adit-jv.com>
2021-09-15 11:09:08 +0900

    lib: Fix wrong type alert from lgtm
    Signed-off-by: Saya Sugiura <ssugiura@jp.adit-jv.com>

--- src/lib/dlt_user.c
+++ src/lib/dlt_user.c
@@ -3878,7 +3878,7 @@ DltReturnValue dlt_user_log_send_log(DltContextData *log, int mtype)
             /* Get file size */
             struct stat st;
             fstat(dlt_user.dlt_log_handle, &st);
-            dlt_vlog(LOG_DEBUG, "%s: Current file size=[%d]\n", __func__,
+            dlt_vlog(LOG_DEBUG, "%s: Current file size=[%ld]\n", __func__,
                      st.st_size);
 
             /* Check filesize */
@@ -3888,7 +3888,7 @@ DltReturnValue dlt_user_log_send_log(DltContextData *log, int mtype)
             if (msg_size > dlt_user.filesize_max) {
                 dlt_user_file_reach_max = true;
                 dlt_vlog(LOG_ERR,
-                         "%s: File size (%d bytes) reached to defined maximum size (%d bytes)\n",
+                         "%s: File size (%ld bytes) reached to defined maximum size (%d bytes)\n",
                          __func__, st.st_size, dlt_user.filesize_max);
                 return DLT_RETURN_FILESZERR;
             }
