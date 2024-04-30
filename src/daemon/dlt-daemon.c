efd8c16bafa8aa14d19333fa345193fc27ad4944
Alexander Mohr <alexander.m.mohr@mercedes-benz.com>
2024-04-30 13:57:23 +0200

    watchdog: fix another possible watchdog timeout (#619)
    dlt_daemon_process_user_message_log prior to this
    commit was running without limit, meaning when a lot of
    messages have to be transmitted dlt-daemon might
    be killed by systemd watchdog due to being busy
    with sending and not updating the watchdog anymore.
    This commit introduces a timeout in dlt_daemon_process_user_message_log
    which allows this function to run at most watchdog_trigger_interval
    seconds.
    
    Signed-off-by: Alexander Mohr <alexander.m.mohr@mercedes-benz.com>

--- src/daemon/dlt-daemon.c
+++ src/daemon/dlt-daemon.c
@@ -3478,7 +3478,22 @@ int dlt_daemon_process_user_message_log(DltDaemon *daemon,
         /* Not enough bytes received to remove*/
         return DLT_DAEMON_ERROR_UNKNOWN;
 
+#ifdef DLT_SYSTEMD_WATCHDOG_ENABLE
+    const unsigned int start_time = dlt_uptime();
+#endif
+
     while (1) {
+#ifdef DLT_SYSTEMD_WATCHDOG_ENABLE
+        const unsigned int uptime = dlt_uptime();
+        if ((uptime - start_time) / 10000 > daemon->watchdog_trigger_interval) {
+            dlt_vlog(LOG_WARNING,
+                     "spent already 1 watchdog trigger interval in %s, yielding to process other events.\n", __func__);
+            if (sd_notify(0, "WATCHDOG=1") < 0)
+                dlt_vlog(LOG_CRIT, "Could not reset systemd watchdog from %s\n", __func__);
+            break;
+        }
+#endif
+
         /* get log message from SHM then store into receiver buffer */
         size = dlt_shm_pull(&(daemon_local->dlt_shm),
                             daemon_local->recv_buf_shm,
