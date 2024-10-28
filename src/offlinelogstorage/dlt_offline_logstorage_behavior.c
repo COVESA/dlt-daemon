d1a7906e7057750eee50cb3ab2d7a5fc4f8bc9dc
gustav <gustav@haleytek.com>
2024-10-28 13:07:57 +0100

    typo

--- src/offlinelogstorage/dlt_offline_logstorage_behavior.c
+++ src/offlinelogstorage/dlt_offline_logstorage_behavior.c
@@ -101,7 +101,7 @@ void dlt_logstorage_log_file_name(char *log_file_name,
 
     const char *suffix =
         ".dlt.gz"; // Unknown here if gzip is used but we assume so as it leads
-                   // to shorter avaialble space
+                   // to shorter available space
     const int smax = DLT_MOUNT_PATH_MAX - strlen(suffix) - 1;
     int spos = 0;
     log_file_name[spos] = '\0';
